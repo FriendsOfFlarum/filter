0012f6d864dd5bf5162aa0996c1aaf1c08597b24
Charlie <issyrocks2020@gmail.com>
2017-04-06 03:35:41 +0000

    Apply fixes from StyleCI

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,6 @@
-<?php namespace issyrocks12\filter;
+<?php
+
+namespace issyrocks12\filter;
 
 use Illuminate\Contracts\Events\Dispatcher;
 use Illuminate\Contracts\View\Factory;
@@ -6,6 +8,6 @@ use Illuminate\Contracts\View\Factory;
 return function (Dispatcher $events, Factory $views) {
     $events->subscribe(Listener\FilterPosts::class);
     $events->subscribe(Listener\AddClientAssets::class);
-  
+
     $views->addNameSpace('issyrocks12-filter', __DIR__.'/views');
 };
--- src/Listener/AddClientAssets.php
+++ src/Listener/AddClientAssets.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace issyrocks12\filter\Listener;
+
 use Flarum\Event\ConfigureWebApp;
 use Illuminate\Contracts\Events\Dispatcher;
 
@@ -13,6 +14,7 @@ class AddClientAssets
     {
         $events->listen(ConfigureWebApp::class, [$this, 'addAssets']);
     }
+
     /**
      * @param ConfigureClientView $event
      */
@@ -21,13 +23,13 @@ class AddClientAssets
         if ($event->isAdmin()) {
             $event->addAssets([
                 __DIR__.'/../../js/admin/dist/extension.js',
-                __DIR__.'/../../less/admin/WordConfigPage.less'
+                __DIR__.'/../../less/admin/WordConfigPage.less',
             ]);
             $event->addBootstrapper('issyrocks12/filter/main');
         }
         if ($event->isForum()) {
             $event->addAssets([
-                __DIR__.'/../../js/forum/dist/extension.js'
+                __DIR__.'/../../js/forum/dist/extension.js',
             ]);
             $event->addBootstrapper('issyrocks12/filter/main');
         }
