49ee41490aa80afb07a62ea481207c95611364b6
Charlie <issyrocks2020@gmail.com>
2017-04-21 17:10:34 +0000

    Apply fixes from StyleCI

--- src/Listener/FilterPosts.php
+++ src/Listener/FilterPosts.php
@@ -3,14 +3,13 @@
 namespace issyrocks12\filter\Listener;
 
 use DirectoryIterator;
-use Flarum\Event\ConfigureLocales;
-use Flarum\Event\PostWillBeSaved;
-use Flarum\Event\PostWasPosted;
-use Flarum\Flags\Flag;
-use Flarum\Core\Post;
-use Flarum\Foundation\Application;
 use Flarum\Core\Post\CommentPost;
 use Flarum\Core\Repository\PostRepository;
+use Flarum\Event\ConfigureLocales;
+use Flarum\Event\PostWasPosted;
+use Flarum\Event\PostWillBeSaved;
+use Flarum\Flags\Flag;
+use Flarum\Foundation\Application;
 use Flarum\Settings\SettingsRepositoryInterface;
 use Illuminate\Contracts\Events\Dispatcher;
 use Illuminate\Mail\Mailer;
@@ -38,7 +37,8 @@ class FilterPosts
     /**
      * @var PostRepository
      */
-    protected $posts;  
+    protected $posts;
+
     /**
      * @param SettingsRepositoryInterface $settings
      * @param Application                 $app
@@ -69,7 +69,6 @@ class FilterPosts
      * @param PostWillBeSaved $event
      */
     public function checkPost(PostWillBeSaved $event)
-      
     {
         $words = explode(', ', $this->settings->get('Words'));
         $post = $event->post;
@@ -86,23 +85,22 @@ class FilterPosts
     }
 
     public function mergePost(PostWasPosted $event)
-    { 
-      $post = $event->post;
-      
-      if ($post instanceof CommentPost && $post->number !== 1 && $this->settings->get('autoMergePosts') == 1) {
-               
+    {
+        $post = $event->post;
+
+        if ($post instanceof CommentPost && $post->number !== 1 && $this->settings->get('autoMergePosts') == 1) {
             $oldPost = $this->posts->query()
               ->where('discussion_id', '=', $post->discussion_id)
               ->where('number', '<', $post->number)
               ->where('hide_time', '=', null)
               ->orderBy('number', 'desc')
               ->firstOrFail();
-        
+
             if ($oldPost->user_id == $post->user_id) {
                 $oldPost->revise($oldPost->content.'
                 
 '.$post->content, $post->user);
-              
+
                 $oldPost->save();
 
                 $post->hide();
