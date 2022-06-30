434cd597e0cf91b80e10d87febd4dbf9d64c7868
David Sevilla Martin <6401250+datitisev@users.noreply.github.com>
2022-06-30 07:29:21 -0400

    Fix censor creation + some maintenance (#32)
    * Update .gitignore, add .gitattributes & fix README image link
    
    * Add tsconfig for core typings, other JS script updates
    
    * Run prettier
    
    * Only use newline character instead of PHP_EOL for user-inputted strings

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -13,15 +13,15 @@ import app from 'flarum/admin/app';
 import FilterSettingsPage from './components/FilterSettingsPage';
 
 app.initializers.add('fof-filter', (app) => {
-    app.extensionData
-        .for('fof-filter')
-        .registerPage(FilterSettingsPage)
-        .registerPermission(
-            {
-                icon: 'fas fa-user-ninja',
-                label: app.translator.trans('fof-filter.admin.permission.bypass_filter_label'),
-                permission: 'discussion.bypassFoFFilter',
-            },
-            'reply'
-        );
+  app.extensionData
+    .for('fof-filter')
+    .registerPage(FilterSettingsPage)
+    .registerPermission(
+      {
+        icon: 'fas fa-user-ninja',
+        label: app.translator.trans('fof-filter.admin.permission.bypass_filter_label'),
+        permission: 'discussion.bypassFoFFilter',
+      },
+      'reply'
+    );
 });
