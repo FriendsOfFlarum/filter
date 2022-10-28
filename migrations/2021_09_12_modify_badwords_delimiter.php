020cc3eaa643cb81e633e38eceb4059f4665816b
David Sevilla Martin <6401250+datitisev@users.noreply.github.com>
2022-10-28 04:22:13 -0400

    fix: migrations assume flarum is already installed (#37)

--- migrations/2021_09_12_modify_badwords_delimiter.php
+++ migrations/2021_09_12_modify_badwords_delimiter.php
@@ -13,14 +13,14 @@ use Illuminate\Database\Schema\Builder;
 
 return [
     'up' => function (Builder $schema) {
-        /**
-         * @var \Flarum\Settings\SettingsRepositoryInterface
-         */
-        $settings = resolve('flarum.settings');
+        $db = $schema->getConnection();
+        $query = $db->table('settings')->where('key', 'fof-filter.words');
+        $result = $query->first();
 
-        if (!empty($words = $settings->get('fof-filter.words', null))) {
-            $words = str_replace(', ', "\n", $words);
-            $settings->set('fof-filter.words', $words);
+        if ($result !== null) {
+            $words = str_replace(', ', "\n", $result->value);
+
+            $query->update(['value' => $words]);
         }
     },
     'down' => function (Builder $schema) {
