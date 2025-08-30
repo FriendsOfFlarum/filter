70e2fd1385c112ad36b13f77d9c264092d1c2c70
David Sevilla Martin <6401250+dsevillamartin@users.noreply.github.com>
2025-08-29 22:16:12 -0400

    fix: ignore empty filter lines to avoid matching everything
    Fixes #51

--- src/CensorGenerator.php
+++ src/CensorGenerator.php
@@ -13,43 +13,45 @@ namespace FoF\Filter;
 
 class CensorGenerator
 {
+    private const LEET_REPLACE = [
+        'a' => '(a|a\.|a\-|4|@|Á|á|À|Â|à|Â|â|Ä|ä|Ã|ã|Å|å|α|Δ|Λ|λ)',
+        'b' => '(b|b\.|b\-|8|\|3|ß|Β|β)',
+        'c' => '(c|c\.|c\-|Ç|ç|¢|€|<|\(|{|©)',
+        'd' => '(d|d\.|d\-|&part;|\|\)|Þ|þ|Ð|ð)',
+        'e' => '(e|e\.|e\-|3|€|È|è|É|é|Ê|ê|∑)',
+        'f' => '(f|f\.|f\-|ƒ)',
+        'g' => '(g|g\.|g\-|6|9)',
+        'h' => '(h|h\.|h\-|Η)',
+        'i' => '(i|i\.|i\-|!|\||\]\[|]|1|∫|Ì|Í|Î|Ï|ì|í|î|ï)',
+        'j' => '(j|j\.|j\-)',
+        'k' => '(k|k\.|k\-|Κ|κ)',
+        'l' => '(l|1\.|l\-|!|\||\]\[|]|£|∫|Ì|Í|Î|Ï)',
+        'm' => '(m|m\.|m\-)',
+        'n' => '(n|n\.|n\-|η|Ν|Π)',
+        'o' => '(o|o\.|o\-|0|Ο|ο|Φ|¤|°|ø)',
+        'p' => '(p|p\.|p\-|ρ|Ρ|¶|þ)',
+        'q' => '(q|q\.|q\-)',
+        'r' => '(r|r\.|r\-|®)',
+        's' => '(s|s\.|s\-|5|\$|§)',
+        't' => '(t|t\.|t\-|Τ|τ|7)',
+        'u' => '(u|u\.|u\-|υ|µ)',
+        'v' => '(v|v\.|v\-|υ|ν)',
+        'w' => '(w|w\.|w\-|ω|ψ|Ψ)',
+        'x' => '(x|x\.|x\-|Χ|χ)',
+        'y' => '(y|y\.|y\-|¥|γ|ÿ|ý|Ÿ|Ý)',
+        'z' => '(z|z\.|z\-|Ζ)',
+    ];
+
     public static function generateCensors(string $wordsList): array
     {
         $badwords = explode("\n", trim($wordsList));
 
-        $leet_replace = [];
-        $leet_replace['a'] = '(a|a\.|a\-|4|@|Á|á|À|Â|à|Â|â|Ä|ä|Ã|ã|Å|å|α|Δ|Λ|λ)';
-        $leet_replace['b'] = '(b|b\.|b\-|8|\|3|ß|Β|β)';
-        $leet_replace['c'] = '(c|c\.|c\-|Ç|ç|¢|€|<|\(|{|©)';
-        $leet_replace['d'] = '(d|d\.|d\-|&part;|\|\)|Þ|þ|Ð|ð)';
-        $leet_replace['e'] = '(e|e\.|e\-|3|€|È|è|É|é|Ê|ê|∑)';
-        $leet_replace['f'] = '(f|f\.|f\-|ƒ)';
-        $leet_replace['g'] = '(g|g\.|g\-|6|9)';
-        $leet_replace['h'] = '(h|h\.|h\-|Η)';
-        $leet_replace['i'] = '(i|i\.|i\-|!|\||\]\[|]|1|∫|Ì|Í|Î|Ï|ì|í|î|ï)';
-        $leet_replace['j'] = '(j|j\.|j\-)';
-        $leet_replace['k'] = '(k|k\.|k\-|Κ|κ)';
-        $leet_replace['l'] = '(l|1\.|l\-|!|\||\]\[|]|£|∫|Ì|Í|Î|Ï)';
-        $leet_replace['m'] = '(m|m\.|m\-)';
-        $leet_replace['n'] = '(n|n\.|n\-|η|Ν|Π)';
-        $leet_replace['o'] = '(o|o\.|o\-|0|Ο|ο|Φ|¤|°|ø)';
-        $leet_replace['p'] = '(p|p\.|p\-|ρ|Ρ|¶|þ)';
-        $leet_replace['q'] = '(q|q\.|q\-)';
-        $leet_replace['r'] = '(r|r\.|r\-|®)';
-        $leet_replace['s'] = '(s|s\.|s\-|5|\$|§)';
-        $leet_replace['t'] = '(t|t\.|t\-|Τ|τ|7)';
-        $leet_replace['u'] = '(u|u\.|u\-|υ|µ)';
-        $leet_replace['v'] = '(v|v\.|v\-|υ|ν)';
-        $leet_replace['w'] = '(w|w\.|w\-|ω|ψ|Ψ)';
-        $leet_replace['x'] = '(x|x\.|x\-|Χ|χ)';
-        $leet_replace['y'] = '(y|y\.|y\-|¥|γ|ÿ|ý|Ÿ|Ý)';
-        $leet_replace['z'] = '(z|z\.|z\-|Ζ)';
+        // If badwords is empty or contains empty strings, it will generate a regex that matches everything.
+        // We need to skip those.
+        $filteredBadwords = array_filter(array_map('trim', $badwords));
 
-        $censorChecks = [];
-        for ($x = 0, $xMax = count($badwords); $x < $xMax; $x++) {
-            $censorChecks[$x] = '/'.str_ireplace(array_keys($leet_replace), array_values($leet_replace), $badwords[$x]).'/i';
-        }
-
-        return $censorChecks;
+        return array_map(static function ($word) {
+            return '/' . str_ireplace(array_keys(self::LEET_REPLACE), array_values(self::LEET_REPLACE), $word) . '/i';
+        }, $filteredBadwords);
     }
 }
